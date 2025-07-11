c5c220398a1a5d16240ceb54cde8002ae8c67a3d
mutatum <www.ronan.c@gmail.com>
2025-07-11 14:09:03 +0200

    Correction of numerical_scheme base class

--- src/scheme/numerical_scheme.hpp
+++ src/scheme/numerical_scheme.hpp
@@ -4,13 +4,17 @@
 #include "../physics/flux_model.hpp"
 #include "../mesh/mesh_1d.hpp" // 1D uniform mesh for now
 
-template <typename State>
+// Resolving Circular Dependency
+template <class SchemePolicy>
+class Field;
+
+template <typename StateType>
 class NumericalScheme
 {
 public:
+    using State = StateType;
     virtual ~NumericalScheme() = default;
 
-    virtual void compute_residual(const Field<State> &solution_field,
-                                  const FluxModel<State> &flux_model,
-                                  Field<State> &R) const = 0;
+    virtual void compute_residual(const Field<NumericalScheme<State>> &solution_field,
+                                  Field<NumericalScheme<State>> &Residual) const = 0;
 };
