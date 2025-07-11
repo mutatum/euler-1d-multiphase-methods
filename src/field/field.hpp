b7523b60763fd58e197d61c2176a3727f2eae21b
mutatum <www.ronan.c@gmail.com>
2025-07-11 13:49:41 +0200

    Completion (theoretical) of field class

--- src/field/field.hpp
+++ src/field/field.hpp
@@ -3,31 +3,27 @@
 #include <stdexcept>
 #include <vector>
 
-template<class SchemePolicy>
-struct Element {
-
-};
-
-template <typename State>
+template <typename SchemePolicy>
 class Field
 {
-
-private:
-    std::vector<Element> data_;
-    const Mesh &mesh_;
-
 public:
+    using Element = typename SchemePolicy::Element;
+    using State = typename SchemePolicy::State;
+    using Scalar = typename State::scalar_type;
     Field(const Mesh &mesh) : mesh_(mesh), data_(mesh.total_cells()) {};
-    Field(const Mesh &mesh, const State &initial_value) : mesh_(mesh), data_(mesh.total_cells(), initial_value) {};
-    Field(const Mesh &mesh, const std::vector<State> &data) : mesh_(mesh), data_(data) {
-        if (data.size() != mesh.total_cells()) {
-            throw std::invalid_argument("Data size does not match the number of cells in the mesh.");
-        }
-    }
+    // Field(const Mesh &mesh, const State &initial_value) : mesh_(mesh), data_(mesh.total_cells(), initial_value) {};
+    // Field(const Mesh &mesh, const std::vector<State> &data) : mesh_(mesh), data_(data) {
+    //     if (data.size() != mesh.total_cells()) {
+    //         throw std::invalid_argument("Data size does not match the number of cells in the mesh.");
+    //     }
+    // }
     std::size_t size() const { return data_.size(); }
 
     const Mesh &get_mesh() const { return this->mesh_; }
 
     const State & operator()(std::size_t i) const {return data_.at(i); }
     State & operator()(std::size_t i) {return data_.at(i); }
+private:
+    std::vector<Element> data_;
+    const Mesh &mesh_;
 };
