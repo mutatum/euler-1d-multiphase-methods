2d63857dd001e4b4eaf1d9fdbd2d48b5b02f8906
Ipse <www.ronan.c@gmail.com>
2025-07-16 23:17:25 +0200

    Refactor DG class: enhance matrix computations, add error handling for mass matrix, and implement flux extraction

--- src/scheme/classic_discontinuous_galerkin.hpp
+++ src/scheme/classic_discontinuous_galerkin.hpp
@@ -1,28 +1,173 @@
 #pragma once
 #include <Eigen/Dense>
-#include "numerical_scheme.hpp"
+#include "../field/field.hpp"
+#include <stdexcept>
+#include <algorithm>
+#include <type_traits>
+#include <iostream>
 
-template <class PhysicsModel, class PolynomialBasis>
-class DG : public NumericalScheme<typename PhysicsModel::State>
+template <class SchemePolicy>
+class Field;
+
+template <class PhysicsModel,
+          class NumericalFlux,
+          class PolynomialBasis,
+          class Quadrature,
+          template <class> class LeftBC,
+          template <class> class RightBC>
+class DG
 {
-private:
+public:
+    using Physics = PhysicsModel;
     using State = typename PhysicsModel::State;
-    using PolynomialOrder = PolynomialBasis::order;
-    Eigen::Matrix<typename State::scalar_type, PolynomialOrder+1, PolynomialOrder+1> S_, M_, M_inv_;
+    using Scalar = typename State::Scalar;
+    static constexpr std::size_t Variables = PhysicsModel::variables;
+    static constexpr std::size_t PolynomialOrder = PolynomialBasis::order;
+
+    struct Element
+    {
+        Eigen::Matrix<Scalar, PolynomialOrder + 1, Variables> coeffs;
+    };
+
+    struct Workspace
+    {
+        std::vector<State> interface_fluxes;
+        std::vector<Eigen::Matrix<Scalar, 2, Variables>> F_star_matrices;
+        Field<DG> projected_fluxes;
+        std::vector<State> ULs, URs;
+
+        explicit Workspace(std::size_t num_cells) : interface_fluxes(num_cells + 1),
+                                                    projected_fluxes(num_cells),
+                                                    F_star_matrices(num_cells),
+                                                    ULs(num_cells + 1),
+                                                    URs(num_cells + 1) {}
+    };
+
+    DG() : S_(compute_S()), M_(compute_M()), B_(compute_B()), M_inv_(M_.inverse()) {}
+
+private:
+    const Eigen::Matrix<Scalar, PolynomialOrder + 1, PolynomialOrder + 1> S_, M_, M_inv_;
+    const Eigen::Matrix<Scalar, PolynomialOrder + 1, 2> B_;
 
     // Stiffness Matrix
-    void compute_S() {
-
+    static Eigen::Matrix<Scalar, PolynomialOrder + 1, PolynomialOrder + 1> compute_S()
+    {
+        Eigen::Matrix<Scalar, PolynomialOrder + 1, PolynomialOrder + 1> S;
+        for (std::size_t j = 0; j < PolynomialOrder + 1; ++j)
+        {
+            for (std::size_t k = 0; k < PolynomialOrder + 1; ++k)
+            {
+                auto integrand = [&](Scalar x) -> Scalar
+                { return PolynomialBasis::evaluate(j, x) * PolynomialBasis::derivative(k, x); };
+                S(j, k) = Quadrature::integrate(integrand, static_cast<Scalar>(-1.0), static_cast<Scalar>(1.0));
+            }
+        }
+        return S;
     }
 
-    void compute_M() {
-        
+    // Mass matrix
+    static Eigen::Matrix<Scalar, PolynomialOrder + 1, PolynomialOrder + 1> compute_M()
+    {
+        Eigen::Matrix<Scalar, PolynomialOrder + 1, PolynomialOrder + 1> M;
+        for (std::size_t j = 0; j < PolynomialOrder + 1; ++j)
+        {
+            for (std::size_t k = 0; k < PolynomialOrder + 1; ++k)
+            {
+                auto integrand = [&](Scalar x)
+                {
+                    return PolynomialBasis::evaluate(j, x) * PolynomialBasis::evaluate(k, x);
+                };
+                M(j, k) = Quadrature::integrate(integrand, static_cast<Scalar>(-1.0), static_cast<Scalar>(1.0));
+            }
+        }
+        // Check if M is invertible
+        if (M.determinant() == 0)
+        {
+            throw std::runtime_error("Mass matrix M is singular and cannot be inverted.");
+        }
+        return M;
+    }
+
+    // Flux extraction Matrix
+    static Eigen::Matrix<Scalar, PolynomialOrder + 1, 2> compute_B()
+    {
+        Eigen::Matrix<Scalar, PolynomialOrder + 1, 2> B;
+        for (std::size_t k = 0; k < PolynomialOrder + 1; ++k)
+        {
+            B(k, 0) = PolynomialBasis::evaluate(k, static_cast<Scalar>(-1.0));
+            B(k, 1) = PolynomialBasis::evaluate(k, static_cast<Scalar>(1.0));
+        }
+        return B;
     }
 
 public:
-    void compute_residual(const Field<State> &solution_field,
-                          Field<State> &R) const override
+    [[nodiscard]] static State evaluate_poly(const Element &element, Scalar xi) noexcept
     {
-        ;
+        State result{};
+        for (std::size_t j = 0; j <= PolynomialOrder; ++j)
+        {
+            result += element.coeffs.row(j) * PolynomialBasis::evaluate(j, xi);
+        }
+        return result;
+    }
+
+    Scalar compute_residual(const Field<DG> &U, Field<DG> &R, Workspace &workspace) const
+    {
+        const std::size_t num_cells = U.size();
+        if (num_cells == 0) {
+            throw std::invalid_argument("Field cannot be empty");
+        }
+
+        // Compute interface fluxes and get max cell speed
+        Scalar max_cell_speed = static_cast<Scalar>(0.0);
+        
+        // Left boundary
+        workspace.ULs[0] = LeftBC<DG>::evaluate(U);
+        workspace.URs[0] = evaluate_poly(U(0), static_cast<Scalar>(-1.0));
+        
+        // Interior interfaces
+        for (std::size_t i = 1; i < num_cells; ++i)
+        {
+            workspace.ULs[i] = evaluate_poly(U(i - 1), static_cast<Scalar>(1.0));
+            workspace.URs[i] = evaluate_poly(U(i), static_cast<Scalar>(-1.0));
+
+            // Computing numerical flux
+            max_cell_speed = std::max(max_cell_speed, 
+                                    NumericalFlux::compute(workspace.ULs[i], workspace.URs[i], workspace.interface_fluxes[i]));
+        }
+        
+        // Right boundary
+        workspace.ULs[num_cells] = evaluate_poly(U(num_cells - 1), static_cast<Scalar>(1.0));
+        workspace.URs[num_cells] = RightBC<DG>::evaluate(U);
+
+        // Project flux onto polynomial basis
+        for (std::size_t i = 0; i < num_cells; ++i)
+        {
+            for (std::size_t j = 0; j <= PolynomialOrder; ++j)
+            {
+                auto integrand = [&](Scalar x) -> State
+                {
+                    return Physics::physical_flux(evaluate_poly(U(i), x)) * PolynomialBasis::evaluate(j, x);
+                };
+                const auto integrated_flux = Quadrature::integrate(integrand, static_cast<Scalar>(-1.0), static_cast<Scalar>(1.0));
+                workspace.projected_fluxes(i).coeffs.row(j) << integrated_flux.density,
+                    integrated_flux.momentum,
+                    integrated_flux.total_energy;
+            }
+            
+            const auto &flux_L = workspace.interface_fluxes[i];
+            workspace.F_star_matrices[i].row(0) << flux_L.density,
+                flux_L.momentum,
+                flux_L.total_energy;
+            
+            const auto &flux_R = workspace.interface_fluxes[i + 1];
+            workspace.F_star_matrices[i].row(1) << -flux_R.density,
+                -flux_R.momentum,
+                -flux_R.total_energy;
+            
+            R(i).coeffs = -M_inv_ * (S_ * workspace.projected_fluxes(i).coeffs - B_ * workspace.F_star_matrices[i]);
+        }
+
+        return max_cell_speed;
     }
 };
