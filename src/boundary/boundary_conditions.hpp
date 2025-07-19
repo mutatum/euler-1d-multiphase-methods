4337dda66c14e61db676781fbffa475f51c7c1b9
Ipse <www.ronan.c@gmail.com>
2025-07-19 19:28:40 +0200

    Refactor boundary condition classes: improve formatting and replace evaluate_poly with evaluate_element for consistency

--- src/boundary/boundary_conditions.hpp
+++ src/boundary/boundary_conditions.hpp
@@ -2,13 +2,17 @@
 #include "../field/field.hpp"
 #include <type_traits>
 
-enum class Side { Left, Right };
+enum class Side
+{
+    Left,
+    Right
+};
 
-template<Side side>
+template <Side side>
 struct PeriodicBC
 {
     template <class Scheme>
-    static typename Scheme::State evaluate(const Field<Scheme>& U)
+    static typename Scheme::State evaluate(const Field<Scheme> &U)
     {
         if constexpr (side == Side::Left)
             return Scheme::evaluate_element(U(U.size() - 1), static_cast<typename Scheme::Scalar>(1.0));
@@ -17,20 +21,33 @@ struct PeriodicBC
     }
 };
 
-template<Side side>
+template <Side side>
+struct CopyBC
+{
+    template <class Scheme>
+    static typename Scheme::State evaluate(const Field<Scheme> &U)
+    {
+        if constexpr (side == Side::Left)
+            return Scheme::evaluate_element(U(0), static_cast<typename Scheme::Scalar>(-1.0));
+        else
+            return Scheme::evaluate_element(U(U.size() - 1), static_cast<typename Scheme::Scalar>(1.0));
+    }
+};
+
+template <Side side>
 struct SolidWallBC
 {
     template <class Scheme>
-    static typename Scheme::State evaluate(const Field<Scheme>& U)
+    static typename Scheme::State evaluate(const Field<Scheme> &U)
     {
         using State = typename Scheme::State;
         using Scalar = typename Scheme::Scalar;
-        
+
         State adjacent_state;
         if constexpr (side == Side::Left)
-            adjacent_state = Scheme::evaluate_poly(U(0), static_cast<Scalar>(1.0));
+            adjacent_state = Scheme::evaluate_element(U(0), static_cast<Scalar>(1.0));
         else
-            adjacent_state = Scheme::evaluate_poly(U(U.size() - 1), static_cast<Scalar>(1.0));
+            adjacent_state = Scheme::evaluate_element(U(U.size() - 1), static_cast<Scalar>(1.0));
 
         return State{
             adjacent_state.density,
