d6f31b5f3fb6acc10017aa723933a35be9ba7d39
Ipse <www.ronan.c@gmail.com>
2025-07-20 16:44:10 +0200

    Refactor Rusanov struct: improve formatting and clarify compute method implementation

--- src/physics/numerical_flux/rusanov.hpp
+++ src/physics/numerical_flux/rusanov.hpp
@@ -2,14 +2,15 @@
 #include <cmath>
 
 template <class PhysicsModel>
-struct Rusanov {
+struct Rusanov
+{
     using Physics = PhysicsModel;
     using State = typename Physics::State;
     using Scalar = typename State::Scalar;
-    
-    static Scalar compute(const State& state_L,
-                         const State& state_R,
-                         State& result)
+
+    static Scalar compute(const State &state_L,
+                          const State &state_R,
+                          State &result)
     {
         const State flux_L = Physics::physical_flux(state_L);
         const State flux_R = Physics::physical_flux(state_R);
@@ -18,7 +19,10 @@ struct Rusanov {
         const Scalar speed_R = Physics::max_wave_speed(state_R);
         const Scalar max_speed = std::max(speed_L, speed_R);
 
-        result = 0.5 * (flux_L + flux_R) - 0.5 * max_speed * (state_R - state_L);
+        result.density = 0.5 * (flux_L.density + flux_R.density) - 0.5 * max_speed * (state_R.density - state_L.density);
+        result.momentum = 0.5 * (flux_L.momentum + flux_R.momentum) - 0.5 * max_speed * (state_R.momentum - state_L.momentum);
+        result.total_energy = 0.5 * (flux_L.total_energy + flux_R.total_energy) - 0.5 * max_speed * (state_R.total_energy - state_L.total_energy);
+
         return max_speed;
     }
 };
